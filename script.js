263f29d6aa7d943b0394ceb424a23bbf25ec5c26
aveencoder <aveen598@gmail.com>
2025-08-01 12:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+function secondHighest(arr){
+   if(arr.length ==0 && arr.length == 1){
+      return -Infinity;
+   }
+   else{
+       function main(arr1){
+     let sarr = arr1.sort((a,b)=> b-a);
+     return sarr[1];
+  }
+   for(let i=0;i<arr.length-1;i++){
+      if(arr[i+1]!=arr[i]){
+            return main(arr);
+      }
+    
+   }
+    return -Infinity;
+
+   }
+}
+
+
